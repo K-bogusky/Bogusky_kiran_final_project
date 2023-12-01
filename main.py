fb11ee0ec9985a50ebac1588c7a7c646305d2163
K-bogusky <142935085+K-bogusky@users.noreply.github.com>
2023-12-01 12:51:53 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This file was created by:Kiran Bogusky
+# Title: The mysterious bell
+# GameDesign:
+    # Goals: for the player:find out who killed the bellarmine bell
+    # Rules: jump and run without falling
+    # Feedback: minimap, interactive menu, timer/day count, sound effects.
+    # Freedom: run side to side, walk through doors in bellarmine's campus.
+# IDEAS:
+# murder simulator(I wrote this last time but to fully explain:
+# murder mystery interactive detective game)
+import pygame as pg
+from pygame import sprite as spr
