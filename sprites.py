e1969c764e009975b14336cbea7e17e1056809d0
K-bogusky <142935085+K-bogusky@users.noreply.github.com>
2023-12-07 10:17:52 -0800

    Modified code base. Pt 1 of update

--- sprites.py
+++ sprites.py
@@ -1,2 +1,21 @@
 # This file was created by:Kiran Bogusky
 # File to hold all sprites classes
+# blocks, libraries, and modules
+import pygame as pg
+from pygame.sprite import Sprite
+from pygame.math import Vector2 as vec
+import os
+from settings import 
+# finding directories
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'images')
+sound_folder = os.path.join(game_folder, 'sounds')
+# global sprites
+
+# creating player class
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        self.game = game
+        if 
+
