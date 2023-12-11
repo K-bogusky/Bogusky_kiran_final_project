a0e069784ac607010ade8f5b60160ee4405309cf
K-bogusky <142935085+K-bogusky@users.noreply.github.com>
2023-12-11 08:50:47 -0800

    expanded Sprites codebase

--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 # This file was created by: Kiran Bogusky
 # This is a file to store all settings, pre-set and manual.
+WIDTH = 1080
+HEIGHT = 720
--- sprites.py
+++ sprites.py
@@ -10,12 +10,32 @@ from settings import *
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 sound_folder = os.path.join(game_folder, 'sounds')
-# global sprites
-
+# global variables
+global locked
 # creating player class
 class Player(Sprite):
+    # from Chris Bradfield at kidscancode
     def __init__(self, game):
         Sprite.__init__(self)
         self.game = game
+        self.image = pg.image.load(os.path.join(img_folder, "player")).convert()
+        self.rect = self.image.get_rect
+        self.rect.center = (0,0)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+    def controls(self):
+        key = pg.key.get_pressed()
+        if key[pg.K_a]:
+            print("temp")
+        if key[pg.K_w]:
+            print("temp")
+        if key[pg.K_s]:
+            print("temp")
+        if key[pg.K_d]:
+            print("temp")
+        if key[pg.K_SCROLLOCK] or key[pg.K_r]:
+            locked = 1
+        
         
 
